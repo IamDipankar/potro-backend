4cb08f9259c0093a4f0a0861e2286231d3cffa81
Dipankar Mitra <dipankar.mitra4854@gmail.com>
2025-09-07 18:46:33 +0600

    a

--- ngl/main.py
+++ ngl/main.py
@@ -24,8 +24,11 @@ app = FastAPI(
 
 # CORS settings
 ALLOWED_ORIGINS = [
-    # "https://potro-backend-1.onrender.com",
-    "*" ### !!! Danger: recheck before production
+    "https://potro-backend-1.onrender.com",
+    # "*" ### !!! Danger: recheck before production
+    "localhost:8000",
+    "localhost:3000",
+    "http://http://127.0.0.1:8000",
 ]
 
 app.add_middleware(
